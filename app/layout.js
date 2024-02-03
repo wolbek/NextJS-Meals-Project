5e7a2dcd33edf579c7b14f5a9c86c30d2aecd4cc
Wolbek <aakashvish22@gmail.com>
2024-02-03 11:45:43 +0530

    Done with using client components efficiently

---
+++ app/layout.js
@@ -0,0 +1,19 @@
+import MainHeaderBackground from '@/components/main-header/main-header-background';
+import './globals.css';
+import MainHeader from '@/components/main-header/main-header';
+
+export const metadata = {
+  title: 'NextLevel Food',
+  description: 'Delicious meals, shared by a food-loving community.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <MainHeader/>
+        {children}
+      </body>
+    </html>
+  );
+}
